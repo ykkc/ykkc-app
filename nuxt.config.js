ee8aed20dd667988efb1bc65c062695a3d26da47
ykkc <ylane.admirable@gmail.com>
2021-03-12 23:13:31 +0900

    :+1: add views with vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,17 @@ module.exports = {
         })
       }
     }
+  },
+  modules: [
+    '@nuxtjs/vuetify'
+  ],
+  vuetify: {
+    theme: {
+      primary: '#3f51b5',
+      secondary: '#b0bec5',
+      accent: '#8c9eff',
+      error: '#b71c1c'
+    }
   }
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,34 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        site
+        ykkc-app
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <v-btn
+          :disabled="loading"
+          class="ma-1"
+          color="grey"
+          plain
+        >
+          About
+        </v-btn>
+        <v-btn
+          :disabled="loading"
+          class="ma-1"
+          color="grey"
+          plain
+        >
+          Work
+        </v-btn>
+        <v-btn
+          :disabled="loading"
+          class="ma-1"
+          color="grey"
+          plain
+        >
+          Contact
+        </v-btn>
       </div>
     </div>
   </section>
@@ -28,6 +40,15 @@ import AppLogo from '~/components/AppLogo.vue'
 export default {
   components: {
     AppLogo
+  },
+  data() {
+    return {
+      title: '',
+      url: '',
+      threads: [],
+      loading: false,
+      filter: ''
+    }
   }
 }
 </script>
