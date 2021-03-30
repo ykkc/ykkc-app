261bdfb4e365e8dc93f2a676db53df1d40f2b004
ykkc <ylane.admirable@gmail.com>
2021-03-31 00:20:49 +0900

    :shower: divided mainpage with foooter

---
+++ components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        :disabled="loading"
+        class="ma-1"
+        color="black"
+        plain
+      >
+        About
+      </v-btn>
+      <v-btn
+        :disabled="loading"
+        class="ma-1"
+        color="black"
+        plain
+      >
+        Work
+      </v-btn>
+      <v-btn
+        :disabled="loading"
+        class="ma-1"
+        color="black"
+        plain
+      >
+        Contact
+      </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,15 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        ykkc-app
-      </h1>
-      <div class="links">
-        <v-btn
-          :disabled="loading"
-          class="ma-1"
-          color="grey"
-          plain
-        >
-          About
-        </v-btn>
-        <v-btn
-          :disabled="loading"
-          class="ma-1"
-          color="grey"
-          plain
-        >
-          Work
-        </v-btn>
-        <v-btn
-          :disabled="loading"
-          class="ma-1"
-          color="grey"
-          plain
-        >
-          Contact
-        </v-btn>
-      </div>
-    </div>
-  </section>
+  <div>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
-    AppLogo
+    Footer
   },
   data() {
     return {
@@ -52,35 +22,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
